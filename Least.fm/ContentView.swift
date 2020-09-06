d7d1afaed1dbd8b5b808d6ffc7b7c3a052c7c538
shavi5w <vivek.aj.sharma@accenture.com>
2020-09-06 09:49:57 +0530

    Initial Commit

---
+++ Least.fm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Least.fm
+//
+//  Created by vivek.aj.sharma on 06/09/20.
+//  Copyright © 2020 vivek.aj.sharma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Least.fmTests/Least_fmTests.swift
@@ -0,0 +1,34 @@
+//
+//  Least_fmTests.swift
+//  Least.fmTests
+//
+//  Created by vivek.aj.sharma on 06/09/20.
+//  Copyright © 2020 vivek.aj.sharma. All rights reserved.
+//
+
+import XCTest
+@testable import Least_fm
+
+class Least_fmTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
